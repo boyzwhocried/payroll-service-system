bf2a3cb6681c2b98ab71fab9b468b524d21b9bf1
StoicCat <josepgultom25@gmail.com>
2024-05-13 15:48:29 +0700

    Initiate Files

---
+++ pss/src/main/java/com/lawencon/pss/PayrollServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lawencon.pss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollServiceSystemApplication.class, args);
+	}
+
+}
